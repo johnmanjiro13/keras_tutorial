c047582a402c7fd0057a68357ebd8392785765b9
johnmanjiro13 <juntama193@gmail.com>
2019-01-04 02:44:34 +0900

    fix type

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -43,5 +43,5 @@ history = model.fit(x_train, y_train,
                     verbose=1,
                     validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss: ', sroce[0])
+print('Test loss: ', score[0])
 print('Test accuracy:', score[1])
