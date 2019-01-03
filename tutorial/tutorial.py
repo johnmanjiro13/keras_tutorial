b4a122b2186a978964a1674e775aff62adbe2f9e
johnmanjiro13 <juntama193@gmail.com>
2019-01-04 02:09:11 +0900

    前処理

---
+++ tutorial/tutorial.py
@@ -0,0 +1,16 @@
+﻿from keras.datasets import mnist
+from keras.utils.np_utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# 画像を１次元化
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+
+# 画素を0〜1の範囲に変換（正規化）
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# 正解ラベルをone-hot-encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
